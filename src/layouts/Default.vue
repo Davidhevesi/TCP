8016d2f264189dae3906442be6c1d157ce047e34
David Hevesi <davidhevesi2008@gmail.com>
2020-04-14 18:28:34 -0400

    first commit created responsive navigation add carousel

---
+++ src/layouts/Default.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="max-w-screen-2xl m-auto">
+    <header class="bg-gray-900 mb-6 sm:flex sm:justify-between sm:">
+      <div class="flex justify-between bg-gray-900 px-4 py-3 mr-10">
+        <span
+          class="shadow-outline px-1 py-1 font-semibold text-xl tracking-tight text-gray-100"
+        >
+          <g-link class="m-2 mr text-white" to="/">{{
+            $static.metadata.siteName
+          }}</g-link></span
+        >
+        <div class="flex sm:hidden">
+          <button
+            @click="isOpen = !isOpen"
+            type="button"
+            class="px-2 text-gray-500 hover:text-white focus:outline-none focus:text-white"
+          >
+            <svg
+              class="h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                v-if="isOpen"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+              />
+              <path
+                v-if="!isOpen"
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <nav
+        :class="isOpen ? 'block' : 'hidden'"
+        class="px-2 pt-2 pb-5 sm:block sm:flex mt-2 mr-10"
+      >
+        <g-link
+          class="nav__link text-white block hover:bg-gray-800"
+          to="/about/"
+          >About</g-link
+        >
+
+        <g-link
+          class="nav__link text-white block hover:bg-gray-800"
+          to="/services/"
+          >Services</g-link
+        >
+        <g-link
+          class="nav__link text-white block hover:bg-gray-800"
+          to="/portfolio/"
+          >Portfolio</g-link
+        >
+        <g-link class="nav__link text-white block hover:bg-gray-800" to="/faqs/"
+          >FAQ'S</g-link
+        >
+        <g-link
+          class="nav__link text-white block hover:bg-gray-800"
+          to="/contact/"
+          >Contact</g-link
+        >
+      </nav>
+    </header>
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+}
+
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
+<script>
+export default {
+  prop: [],
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    }
+  }
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,125 @@
+<template>
+  <Layout>
+    <Carousel :per-page="1" class="-mt-6">
+      <slide>
+        <g-image src="~/layouts/img/Car2.jpg" class="h-full" />
+      </slide>
+      <slide>
+        <g-image src="~/layouts/img/Motocycle.jpg" class="h-full" />
+      </slide>
+      <slide>
+        <g-image src="~/layouts/img/Porche.jpg" class="h-full" />
+      </slide>
+    </Carousel>
+    <div class="">
+      <h1 class="text-center">featured services</h1>
+    </div>
+
+    <div class="flex flex-col mb-4 md:flex md:flex-row">
+      <div class="md:w-1/2 h-12 md:ml-12 mb-20">
+        <div class="px-2 py-2">
+          <h1 class="text-center">COLOR CHANGE VINYL WRAPS</h1>
+          <p class="text-center px-2 py-2">
+            Full single color or customized vinyl wrapping services for cars,
+            bikes and boats.
+          </p>
+          <div class="text-center">
+            <span class="inline-flex rounded-md shadow-sm">
+              <button
+                type="button"
+                class=" mb-4 inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+              >
+                More Details
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="md:w-1/2 h-12">
+        <div class="md:w-1/2 h-12 md:ml-12">
+          <div class="">
+            <h1 class="text-center">COLOR CHANGE VINYL WRAPS</h1>
+            <p class="text-center">
+              Full single color or customized vinyl wrapping services for cars,
+              bikes and boats.
+            </p>
+            <div class="text-center">
+              <span class="inline-flex rounded-md shadow-sm">
+                <button
+                  type="button"
+                  class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+                >
+                  More Details
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="md:w-1/2 bg-gray-600 h-12">
+        <div class="md:w-1/2 h-12 md:ml-12">
+          <div class="">
+            <h1 class="text-center">COLOR CHANGE VINYL WRAPS</h1>
+            <p class="text-center">
+              Full single color or customized vinyl wrapping services for cars,
+              bikes and boats.
+            </p>
+            <div class="text-center">
+              <span class="inline-flex rounded-md shadow-sm">
+                <button
+                  type="button"
+                  class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+                >
+                  More Details
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="md:w-1/2 h-12 mr-12">
+        <div class="md:w-1/2 h-12 md:ml-12">
+          <div class="">
+            <h1 class="text-center">COLOR CHANGE VINYL WRAPS</h1>
+            <p class="text-center">
+              Full single color or customized vinyl wrapping services for cars,
+              bikes and boats.
+            </p>
+            <div class="text-center">
+              <span class="inline-flex rounded-md shadow-sm">
+                <button
+                  type="button"
+                  class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+                >
+                  More Details
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import { Carousel, Slide } from "vue-carousel";
+
+export default {
+  data() {
+    return {
+      /* data properties here */
+    };
+  },
+  components: {
+    Carousel,
+    Slide
+  }
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
